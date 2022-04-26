f64316ae6c990eb19c257894b9a1d6665eea1878
Minsoo Kim <garlicdipping101@gmail.com>
2022-04-26 17:10:29 +0900

    각종 수학함수 브릿지 추가

--- unity-plugin/fpm-unity.cpp
+++ unity-plugin/fpm-unity.cpp
@@ -33,6 +33,12 @@ FixedFloat divide(FixedFloat a_raw, FixedFloat b_raw)
     return a_raw;
 }
 
+void from_int(FixedFloat* in, fixed_float_base_type integer)
+{
+    fixed_float res {integer};
+    in->rawValue = res.raw_value();
+}
+
 void mix(FixedFloat* in, fixed_float_base_type integer, fixed_float_base_type numerator, fixed_float_base_type denominator)
 {
     fixed_float res = fixed_float::mix(integer, numerator, denominator);
@@ -46,10 +52,80 @@ float to_float(FixedFloat raw)
 
 //Mathmatics
 
-void pow(FixedFloat* in, FixedFloat base, FixedFloat pow)
+void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow)
 {
     auto ffBase = fixed_float::from_raw_value(base.rawValue);
     auto ffPow = fixed_float::from_raw_value(pow.rawValue);
     auto res = fpm::pow(ffBase, ffPow);
     in->rawValue = res.raw_value();
+}
+
+void ff_sqrt(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::sqrt(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_sin(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::sin(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_cos(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::cos(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_tan(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::tan(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_asin(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::asin(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_acos(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::acos(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void ff_atan(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::atan(ffVal);
+    in->rawValue = res.raw_value();
+}
+
+void e(FixedFloat* out)
+{
+    auto e = fixed_float::e();
+    out->rawValue = e.raw_value();
+}
+void pi(FixedFloat* out)
+{
+    auto pi = fixed_float::pi();
+    out->rawValue = pi.raw_value();
+}
+void half_pi(FixedFloat* out)
+{
+    auto half_pi = fixed_float::half_pi();
+    out->rawValue = half_pi.raw_value();
+}
+void two_pi(FixedFloat* out)
+{
+    auto two_pi = fixed_float::two_pi();
+    out->rawValue = two_pi.raw_value();
 }
--- unity-plugin/include/fpm-unity.h
+++ unity-plugin/include/fpm-unity.h
@@ -12,7 +12,7 @@
 #include "fpm/ios.hpp"    // For fpm::operator<<
 
 using fixed_float_base_type = std::int32_t;
-using fixed_float = fpm::fixed<fixed_float_base_type, std::int64_t, 12>; //Q20.12
+using fixed_float = fpm::fixed<fixed_float_base_type, std::int64_t, 16>; //Q16.16
 
 extern "C"
 {
@@ -25,8 +25,26 @@ DLLExport FixedFloat add(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat multiply(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat subtract(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat divide(FixedFloat a_raw, FixedFloat b_raw);
+DLLExport void from_int(FixedFloat* in, fixed_float_base_type integer);
 DLLExport void mix(FixedFloat* in, fixed_float_base_type integer, fixed_float_base_type numerator, fixed_float_base_type denominator);
 DLLExport float to_float(FixedFloat raw);
+
+//Mathmatics
+
+DLLExport void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow);
+DLLExport void ff_sqrt(FixedFloat* in, FixedFloat val);
+DLLExport void ff_sin(FixedFloat* in, FixedFloat val);
+DLLExport void ff_cos(FixedFloat* in, FixedFloat val);
+DLLExport void ff_tan(FixedFloat* in, FixedFloat val);
+DLLExport void ff_asin(FixedFloat* in, FixedFloat val);
+DLLExport void ff_acos(FixedFloat* in, FixedFloat val);
+DLLExport void ff_atan(FixedFloat* in, FixedFloat val);
+
+//NEED CACHING ON C# STRUCT INITIALIZATION!
+DLLExport void e(FixedFloat* out);
+DLLExport void pi(FixedFloat* out);
+DLLExport void half_pi(FixedFloat* out);
+DLLExport void two_pi(FixedFloat* out);
 }
 
 #endif //FPM_FPM_UNITY_H
