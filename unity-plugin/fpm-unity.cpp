7a0033135914ace917e32066603b4b5f1c498cd2
Minsoo Kim <garlicdipping101@gmail.com>
2022-04-27 15:24:26 +0900

    fixed point 수학함수 대부분 래핑

--- unity-plugin/fpm-unity.cpp
+++ unity-plugin/fpm-unity.cpp
@@ -4,6 +4,8 @@
 
 #include "include/fpm-unity.h"
 
+#pragma region Arithmatics
+
 FixedFloat add(FixedFloat a_raw, FixedFloat b_raw)
 {
     fixed_float res = fixed_float::from_raw_value(a_raw.rawValue)
@@ -33,6 +35,10 @@ FixedFloat divide(FixedFloat a_raw, FixedFloat b_raw)
     return a_raw;
 }
 
+#pragma endregion
+
+#pragma region Utilities
+
 void from_int(FixedFloat* in, fixed_float_base_type integer)
 {
     fixed_float res {integer};
@@ -50,7 +56,70 @@ float to_float(FixedFloat raw)
     return (float)fixed_float::from_raw_value(raw.rawValue);
 }
 
-//Mathmatics
+#pragma endregion
+
+#pragma region Mathmatics
+
+
+void ff_ceil(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::ceil(ffVal);
+    in->rawValue = res.raw_value();
+}
+void ff_floor(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::floor(ffVal);
+    in->rawValue = res.raw_value();
+}
+void ff_trunc(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::trunc(ffVal);
+    in->rawValue = res.raw_value();
+}
+void ff_round(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::round(ffVal);
+    in->rawValue = res.raw_value();
+}
+void ff_abs(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::abs(ffVal);
+    in->rawValue = res.raw_value();
+}
+void ff_fmod(FixedFloat* in, FixedFloat x, FixedFloat y)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto ffY = fixed_float::from_raw_value(y.rawValue);
+    auto res = fpm::fmod(ffX, ffY);
+    in->rawValue = res.raw_value();
+}
+void ff_remainder(FixedFloat* in, FixedFloat x, FixedFloat y)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto ffY = fixed_float::from_raw_value(y.rawValue);
+    auto res = fpm::remainder(ffX, ffY);
+    in->rawValue = res.raw_value();
+}
+void ff_copysign(FixedFloat* in, FixedFloat x, FixedFloat y)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto ffY = fixed_float::from_raw_value(y.rawValue);
+    auto res = fpm::copysign(ffX, ffY);
+    in->rawValue = res.raw_value();
+}
+void ff_modf(FixedFloat* in, FixedFloat val, FixedFloat* iPtr)
+{
+    fixed_float iVal;
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::modf(ffVal, &iVal);
+    in->rawValue = res.raw_value();
+    iPtr->rawValue = iVal.raw_value();
+}
 
 void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow)
 {
@@ -60,12 +129,59 @@ void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow)
     in->rawValue = res.raw_value();
 }
 
+void ff_exp(FixedFloat* in, FixedFloat x)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::exp(ffX);
+    in->rawValue = res.raw_value();
+}
+void ff_exp2(FixedFloat* in, FixedFloat x)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::exp2(ffX);
+    in->rawValue = res.raw_value();
+}
+void ff_log2(FixedFloat* in, FixedFloat x)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::log2(ffX);
+    in->rawValue = res.raw_value();
+}
+void ff_ln(FixedFloat* in, FixedFloat x)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::log(ffX);
+    in->rawValue = res.raw_value();
+}
+void ff_log10(FixedFloat* in, FixedFloat x)
+{
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::log10(ffX);
+    in->rawValue = res.raw_value();
+}
+
+void ff_cbrt(FixedFloat* in, FixedFloat val)
+{
+    auto ffVal = fixed_float::from_raw_value(val.rawValue);
+    auto res = fpm::cbrt(ffVal);
+    in->rawValue = res.raw_value();
+}
 void ff_sqrt(FixedFloat* in, FixedFloat val)
 {
     auto ffVal = fixed_float::from_raw_value(val.rawValue);
     auto res = fpm::sqrt(ffVal);
     in->rawValue = res.raw_value();
 }
+
+
+
+
+
+
+
+#pragma endregion
+
+#pragma region Trigonometrics
 
 void ff_sin(FixedFloat* in, FixedFloat val)
 {
@@ -109,6 +225,18 @@ void ff_atan(FixedFloat* in, FixedFloat val)
     in->rawValue = res.raw_value();
 }
 
+void ff_atan2(FixedFloat* in, FixedFloat y, FixedFloat x)
+{
+    auto ffY = fixed_float::from_raw_value(y.rawValue);
+    auto ffX = fixed_float::from_raw_value(x.rawValue);
+    auto res = fpm::atan2(ffY, ffX);
+    in->rawValue = res.raw_value();
+}
+
+#pragma endregion
+
+#pragma region Predefined variables
+
 void e(FixedFloat* out)
 {
     auto e = fixed_float::e();
@@ -129,3 +257,5 @@ void two_pi(FixedFloat* out)
     auto two_pi = fixed_float::two_pi();
     out->rawValue = two_pi.raw_value();
 }
+
+#pragma endregion
--- unity-plugin/include/fpm-unity.h
+++ unity-plugin/include/fpm-unity.h
@@ -25,20 +25,44 @@ DLLExport FixedFloat add(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat multiply(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat subtract(FixedFloat a_raw, FixedFloat b_raw);
 DLLExport FixedFloat divide(FixedFloat a_raw, FixedFloat b_raw);
+
+//Utilities
 DLLExport void from_int(FixedFloat* in, fixed_float_base_type integer);
 DLLExport void mix(FixedFloat* in, fixed_float_base_type integer, fixed_float_base_type numerator, fixed_float_base_type denominator);
 DLLExport float to_float(FixedFloat raw);
 
-//Mathmatics
+//trigonometrics
 
-DLLExport void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow);
-DLLExport void ff_sqrt(FixedFloat* in, FixedFloat val);
 DLLExport void ff_sin(FixedFloat* in, FixedFloat val);
 DLLExport void ff_cos(FixedFloat* in, FixedFloat val);
 DLLExport void ff_tan(FixedFloat* in, FixedFloat val);
 DLLExport void ff_asin(FixedFloat* in, FixedFloat val);
 DLLExport void ff_acos(FixedFloat* in, FixedFloat val);
 DLLExport void ff_atan(FixedFloat* in, FixedFloat val);
+DLLExport void ff_atan2(FixedFloat* in, FixedFloat y, FixedFloat x);
+
+//Mathmatics
+
+DLLExport void ff_ceil(FixedFloat* in, FixedFloat val);
+DLLExport void ff_floor(FixedFloat* in, FixedFloat val);
+DLLExport void ff_trunc(FixedFloat* in, FixedFloat val);
+DLLExport void ff_round(FixedFloat* in, FixedFloat val);
+DLLExport void ff_abs(FixedFloat* in, FixedFloat val);
+DLLExport void ff_fmod(FixedFloat* in, FixedFloat x, FixedFloat y);
+DLLExport void ff_remainder(FixedFloat* in, FixedFloat x, FixedFloat y);
+DLLExport void ff_copysign(FixedFloat* in, FixedFloat x, FixedFloat y);
+DLLExport void ff_modf(FixedFloat* in, FixedFloat val, FixedFloat* iPtr);
+
+DLLExport void ff_pow(FixedFloat* in, FixedFloat base, FixedFloat pow);
+DLLExport void ff_exp(FixedFloat* in, FixedFloat x);
+DLLExport void ff_exp2(FixedFloat* in, FixedFloat x);
+DLLExport void ff_log2(FixedFloat* in, FixedFloat x);
+DLLExport void ff_ln(FixedFloat* in, FixedFloat x);
+DLLExport void ff_log10(FixedFloat* in, FixedFloat x);
+
+DLLExport void ff_cbrt(FixedFloat* in, FixedFloat val);
+DLLExport void ff_sqrt(FixedFloat* in, FixedFloat val);
+
 
 //NEED CACHING ON C# STRUCT INITIALIZATION!
 DLLExport void e(FixedFloat* out);
